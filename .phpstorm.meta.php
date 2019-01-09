4be19ff44e73c1b9067148dc56f28a5b7fbc99e0
AndriaSliskovic <andriasliskovic@gmail.com>
2019-01-09 10:54:58 +0100

    Promena meta fajla

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-12-11 12:18:50.
+    * Generated on 2019-01-02 07:24:13.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.10 on 2018-12-11 12:18:49.
+ * Generated for Laravel 5.7.10 on 2019-01-02 07:24:11.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
